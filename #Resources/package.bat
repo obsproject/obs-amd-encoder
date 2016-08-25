7d2990ccd6e365b7eb10158967b21086f9820f07
Michael Fabian Dirks <michael.dirks@xaymar.com>
2016-08-25 22:17:55 +0200

    project: Update package.bat.

--- #Resources/package.bat
+++ #Resources/package.bat
@@ -1,5 +1,4 @@
 REM Shut up, I'm just as lazy as anyone else.
 "C:\Program Files\7-Zip\7z" a -t7z -mx=9 -m0=lzma2 -aoa -mfb=64 -md=32m -ms=on  "Output/OBS-AMD-AMF-Plugin.7z" "../enc-amf/#Build/Release/*"
 "C:\Program Files\7-Zip\7z" a -tzip -mx=9 "Output/OBS-AMD-AMF-Plugin.zip" "../enc-amf/#Build/Release/*"
-Pause
 "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "Installer.iss"
