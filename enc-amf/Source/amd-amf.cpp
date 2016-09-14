ef62a383de95b311939ccd561a69dd554ad24dbd
Michael Fabian Dirks <michael.dirks@xaymar.com>
2016-09-14 21:05:01 +0200

    amd-amf: Change how AMF traces are written to the log file.

--- enc-amf/Source/amd-amf.cpp
+++ enc-amf/Source/amd-amf.cpp
@@ -43,10 +43,10 @@ using namespace Plugin::AMD;
 class CustomWriter : public amf::AMFTraceWriter {
 	public:
 	virtual void Write(const wchar_t* scope, const wchar_t* message) override {
-		const wchar_t* realmsg = &(message[(33 + wcslen(scope) + 2)]);
-		size_t msgLen = wcslen(realmsg) - (sizeof(wchar_t));
+		//const wchar_t* realmsg = &(message[(33 + wcslen(scope) + 2)]);
+		size_t msgLen = wcslen(message) - (sizeof(wchar_t));
 
-		blog(LOG_INFO, "[AMFTrace::%ls] %.*ls", scope, msgLen, realmsg);
+		blog(LOG_INFO, "[AMFTrace] %.*ls", msgLen, message);
 	}
 
 	virtual void Flush() override {}
