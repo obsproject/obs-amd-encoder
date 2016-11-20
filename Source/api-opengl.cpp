43055e68436d6f32f6141747a2ec9ed15d4ba63b
Michael Fabian Dirks <info@xaymar.com>
2016-11-20 17:21:10 +0100

    api-opengl: Implement basic OpenGL API code.

---
+++ Source/api-opengl.cpp
@@ -0,0 +1,66 @@
+/*
+MIT License
+
+Copyright (c) 2016 Michael Fabian Dirks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+// Includes
+//////////////////////////////////////////////////////////////////////////
+#include "api-opengl.h"
+
+#include <vector>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <gl/GL.h>
+
+//////////////////////////////////////////////////////////////////////////
+// Code
+//////////////////////////////////////////////////////////////////////////
+using namespace Plugin::API;
+
+std::string Plugin::API::OpenGL::GetName() {
+	return std::string("OpenGL");
+}
+
+std::vector<Adapter> Plugin::API::OpenGL::EnumerateAdapters() {
+	std::vector<Adapter> adapters;
+	adapters.push_back(Adapter(0, 0, "Default"));
+	return adapters;
+}
+
+void* Plugin::API::OpenGL::CreateInstanceOnAdapter(Adapter adapter) {
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+void Plugin::API::OpenGL::DestroyInstance(void* instance) {
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+Plugin::API::Adapter Plugin::API::OpenGL::GetAdapterForInstance(void* instance) {
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+void* Plugin::API::OpenGL::GetContextFromInstance(void* instance) {
+	throw std::logic_error("The method or operation is not implemented.");
+}
