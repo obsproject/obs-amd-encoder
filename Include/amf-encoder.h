7ae7ae0c9c88fdd501c9f3386efda379e35743b7
Michael Fabian Dirks <info@xaymar.com>
2017-01-28 00:24:04 +0100

    Encoder: Add shared class for AVC, SVC & HEVC

---
+++ Include/amf-encoder.h
@@ -0,0 +1,308 @@
+/*
+MIT License
+
+Copyright (c) 2016 Michael Fabian Dirks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+#include "plugin.h"
+#include "amf.h"
+#include "api-base.h"
+
+#include "components/VideoConverter.h"
+#include "components/VideoEncoderVCE.h"
+#include "components/VideoEncoderHEVC.h"
+
+#define AMF_PRESENT_TIMESTAMP L"PTS"
+#define AMF_SUBMIT_TIMESTAMP L"STS"
+
+namespace Plugin {
+	namespace AMD {
+		// Initialization Parameters
+		enum class Codec : uint8_t {
+			H264AVC,
+			H264SVC,
+			HEVC,
+		};
+		enum class ColorFormat : uint8_t {
+			/*	Support Table
+			 *	Open Broadcaster	AMD AMF
+			 *	-- 4:2:0 Formats --
+			 *	VIDEO_FORMAT_I420	AMF_SURFACE_YUV420P
+			 *	VIDEO_FORMAT_NV12	AMF_SURFACE_NV12
+			 *						AMF_SURFACE_YV12
+			 *	-- 4:2:2 Formats --
+			 *	VIDEO_FORMAT_YVYV
+			 *	VIDEO_FORMAT_YUY2	AMF_SURFACE_YUY2
+			 *	VIDEO_FORMAT_UYVY
+			 *
+			 *	-- 4:4:4 Formats --
+			 *	VIDEO_FORMAT_I444
+			 *
+			 *	-- Packed Uncompressed Formats --
+			 *	VIDEO_FORMAT_RGBA	AMF_SURFACE_RGBA
+			 *	VIDEO_FORMAT_BGRA	AMF_SURFACE_BGRA
+			 *						AMF_SURFACE_ARGB
+			 *	VIDEO_FORMAT_BGRX
+			 *
+			 *	-- Single/Dual Channel Formats --
+			 *	VIDEO_FORMAT_Y800	AMF_SURFACE_GRAY8
+			 *						AMF_SURFACE_U8V8
+			 *
+			 *	-- HDR Color Formats --
+			 *						AMF_SURFACE_P010
+			 *						AMF_SURFACE_RGBA_F16
+			 */
+
+			I420,
+			NV12,
+			YUY2,
+			BGRA,
+			RGBA,
+			GRAY,
+		};
+		enum class ColorSpace : uint8_t {
+			BT609,
+			BT701,
+			BT2020,
+		};
+
+		// Properties
+		enum class Usage : uint8_t {
+			Transcoding,
+			UltraLowLatency,
+			LowLatency,
+			Webcam
+		};
+		enum class QualityPreset : uint8_t {
+			Speed,
+			Balanced,
+			Quality,
+		};
+		enum class Profile : uint16_t {
+			ConstrainedBaseline,
+			Baseline,
+			Main,
+			ConstrainedHigh,
+			High,
+		};
+		enum class ProfileLevel : uint8_t {
+			Automatic,
+			L10 = 10,
+			L11,
+			L12,
+			L13,
+			L20 = 20,
+			L21,
+			L22,
+			L30 = 30,
+			L31,
+			L32,
+			L40 = 40,
+			L41,
+			L42,
+			L50 = 50,
+			L51,
+			L52,
+			L60 = 60,
+			L61,
+			L62,
+		};
+		enum class CodingType : uint8_t {
+			Automatic,
+			CALVC,
+			CABAC,
+		};
+		enum class RateControlMethod : uint8_t {
+			ConstantQP,
+			LatencyConstrainedVariableBitrate,
+			PeakConstrainedVariableBitrate,
+			ConstantBitrate,
+		};
+		enum class PrePassMode : uint8_t {
+			Disabled,
+			Enabled,
+			EnabledAtHalfScale,
+			EnabledAtQuarterScale,
+		};
+
+		class Encoder {
+			protected:
+			Encoder(Codec codec,
+				std::string videoAPI, uint64_t videoAdapterId, bool useOpenCL,
+				ColorFormat colorFormat, ColorSpace colorSpace, bool fullRangeColor);
+			public:
+			virtual ~Encoder();
+
+			public:
+
+			// Properties - Initialization
+			virtual std::vector<Usage> CapsUsage() = 0;
+			virtual void SetUsage(Usage v) = 0;
+			virtual Usage GetUsage() = 0;
+
+			// Properties - Static
+			virtual std::vector<QualityPreset> CapsQualityPreset() = 0;
+			virtual void SetQualityPreset(QualityPreset v) = 0;
+			virtual QualityPreset GetQualityPreset() = 0;
+
+			virtual std::vector<Profile> CapsProfile() = 0;
+			virtual void SetProfile(Profile v) = 0;
+			virtual Profile GetProfile() = 0;
+
+			virtual std::vector<ProfileLevel> CapsProfileLevel() = 0;
+			virtual void SetProfileLevel(ProfileLevel v) = 0;
+			virtual ProfileLevel GetProfileLevel() = 0;
+			
+			virtual std::pair<uint64_t, uint64_t> CapsMaximumReferenceFrames() = 0;
+			virtual void SetMaximumReferenceFrames(uint64_t v) = 0;
+			virtual uint64_t GetMaximumReferenceFrames() = 0;
+
+			virtual std::pair<std::pair<uint32_t, uint32_t>, std::pair<uint32_t, uint32_t>> CapsResolution() = 0;
+			virtual void SetResolution(std::pair<uint32_t, uint32_t> v) = 0;
+			virtual std::pair<uint32_t, uint32_t> GetResolution() = 0;
+
+			virtual void SetAspectRatio(std::pair<uint32_t, uint32_t> v) = 0;
+			virtual std::pair<uint32_t, uint32_t> GetAspectRatio() = 0;
+
+			virtual void SetFrameRate(std::pair<uint32_t, uint32_t> v) = 0;
+			virtual std::pair<uint32_t, uint32_t> GetFrameRate() = 0;
+			protected:
+			void UpdateFrameRateValues();
+			public:
+
+			virtual std::vector<CodingType> CapsCodingType() = 0;
+			virtual void SetCodingType(CodingType v) = 0;
+			virtual CodingType GetCodingType() = 0;
+			
+			// Properties - Dynamic
+			virtual std::vector<RateControlMethod> CapsRateControlMethod() = 0;
+			virtual void SetRateControlMethod(RateControlMethod v) = 0;
+			virtual RateControlMethod GetRateControlMethod() = 0;
+
+			virtual std::vector<PrePassMode> CapsPrePassMode() = 0;
+			virtual void SetPrePassMode(PrePassMode v) = 0;
+			virtual PrePassMode GetPrePassMode() = 0;
+
+			virtual void SetVariableBitrateAverageQualityEnabled(bool v) = 0;
+			virtual bool IsVariableBitrateAverageQualityEnabled() = 0;
+
+			virtual void SetFrameSkippingEnabled(bool v) = 0;
+			virtual bool IsFrameSkippingEnabled() = 0;
+
+			/// Enforce Hypothethical Reference Decoder Restrictions
+			virtual void SetEnforceHRDEnabled(bool v) = 0;
+			virtual bool IsEnforceHRDEnabled() = 0;
+
+			virtual std::pair<uint64_t, uint64_t> CapsTargetBitrate() = 0;
+			virtual void SetTargetBitrate(uint64_t v) = 0;
+			virtual uint64_t GetTargetBitrate() = 0;
+
+			virtual std::pair<uint64_t, uint64_t> CapsPeakBitrate() = 0;
+			virtual void SetPeakBitrate(uint64_t v) = 0;
+			virtual uint64_t GetPeakBitrate() = 0;
+
+			virtual void SetIFrameQP(uint8_t v) = 0;
+			virtual uint8_t GetIFrameQP() = 0;
+
+			virtual void SetPFrameQP(uint8_t v) = 0;
+			virtual uint8_t GetPFrameQP() = 0;
+
+			virtual std::pair<uint64_t, uint64_t> CapsVBVBufferSize() = 0;
+			virtual void SetVBVBufferSize(uint64_t v) = 0;
+			virtual uint64_t GetVBVBufferSize() = 0;
+
+			virtual void SetVBVBufferInitialFullness(float v) = 0;
+			virtual float GetInitialVBVBufferFullness() = 0;
+
+			// Properties - Picture Control
+			virtual void SetIDRPeriod(uint64_t v) = 0;
+			virtual uint64_t GetIDRPeriod() = 0;
+			// Perhaps add utility function for Keyframe Interval? Simplifies stuff.
+			// Header Insertion Spacing?
+
+			virtual void SetDeblockingFilterEnabled(bool v) = 0;
+			virtual bool IsDeblockingFilterEnabled() = 0;
+
+			// Properties - Motion Estimation
+			virtual void SetMotionEstimationQuarterPixelEnabled(bool v) = 0;
+			virtual bool GetMotionEstimationQuarterPixelEnabled() = 0;
+			virtual void SetMotionEstimationHalfPixelEnabled(bool v) = 0;
+			virtual bool GetMotionEstimationHalfPixelEnabled() = 0;
+
+			// Unknown:
+			// Intra-Refresh stuff
+			// Slicing stuff
+			// ConstraintSetFlags (H264 only?)
+
+			// Control
+			void Start();
+			void Restart();
+			void Stop();
+
+			bool Encode(struct encoder_frame* f, struct encoder_packet* p, bool* b);
+
+			protected:
+			uint64_t m_UniqueId;
+
+			// AMF Internals
+			Plugin::AMD::AMF* m_AMF;
+			amf::AMFFactory* m_AMFFactory;
+			amf::AMFContextPtr m_AMFContext;
+			amf::AMFComputePtr m_AMFCompute;
+			amf::AMFComponentPtr m_AMFEncoder;
+			amf::AMFComponentPtr m_AMFConverter;
+			amf::AMF_MEMORY_TYPE m_AMFMemoryType;
+			amf::AMF_SURFACE_FORMAT m_AMFSurfaceFormat;
+			std::vector<uint8_t> m_PacketDataBuffer;
+
+			// API Related
+			std::shared_ptr<API::Base> m_API;
+			API::Adapter m_APIAdapter;
+			void* m_APIDevice;
+
+			// Properties
+			Codec m_Codec;
+			ColorFormat m_ColorFormat;
+			ColorSpace m_ColorSpace;
+			bool m_FullColorRange;
+
+			std::pair<uint32_t, uint32_t> m_Resolution;
+			std::pair<uint32_t, uint32_t> m_FrameRate;
+			uint64_t m_FrameRateTimeStepAMF;
+			double_t m_FrameRateTimeStep;
+
+			// Flags
+			bool m_Started;
+			bool m_OpenCLSubmission;
+			bool m_OpenCLConversion;
+
+		};
+	}
+}
---
+++ Source/amf-encoder.cpp
@@ -0,0 +1,514 @@
+/*
+MIT License
+
+Copyright (c) 2016 Michael Fabian Dirks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "amf-encoder.h"
+#include "misc-util.cpp"
+
+using namespace Plugin;
+using namespace Plugin::AMD;
+
+static const wchar_t* fullColorParams[] = {
+	L"FullRangeColor",
+	L"NominalRange",
+};
+
+Plugin::AMD::Encoder::Encoder(Codec codec,
+	std::string videoAPI, uint64_t videoAdapterId, bool useOpenCL,
+	ColorFormat colorFormat, ColorSpace colorSpace, bool fullRangeColor) {
+	#pragma region Null Values
+	m_UniqueId = Utility::GetUniqueIdentifier();
+	/// AMF Internals
+	m_AMF = nullptr;
+	m_AMFFactory = nullptr;
+	m_AMFContext = nullptr;
+	m_AMFEncoder = nullptr;
+	m_AMFConverter = nullptr;
+	m_AMFMemoryType = amf::AMF_MEMORY_UNKNOWN;
+	m_AMFSurfaceFormat = amf::AMF_SURFACE_UNKNOWN;
+	/// API Related
+	m_API = nullptr;
+	m_APIDevice = nullptr;
+	m_OpenCLSubmission = false;
+	/// Properties
+	m_Codec = codec;
+	m_ColorFormat = colorFormat;
+	m_ColorSpace = colorSpace;
+	m_FullColorRange = fullRangeColor;
+	m_Resolution = std::make_pair<uint32_t, uint32_t>(0, 0);
+	m_FrameRate = std::make_pair<uint32_t, uint32_t>(0, 0);
+	m_FrameRateTimeStepAMF = 0;
+	/// Flags
+	m_Started = false;
+	m_OpenCLConversion = false;
+	#pragma endregion Null Values
+
+	// Initialize selected API on Video Adapter
+	m_API = API::Base::GetAPIByName(videoAPI);
+	m_APIAdapter = m_API->GetAdapterById(videoAdapterId & UINT_MAX, (videoAdapterId >> 32) & UINT_MAX);
+	m_APIDevice = m_API->CreateInstanceOnAdapter(m_APIAdapter);
+
+	// Initialize Advanced Media Framework
+	m_AMF = AMF::GetInstance();
+	/// Retrieve Factory
+	m_AMFFactory = m_AMF.lock()->GetFactory();
+
+	// Create Context for Conversion and Encoding
+	AMF_RESULT res = m_AMFFactory->CreateContext(&m_AMFContext);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Creating a AMF Context failed, error %ls (code %d).",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	/// Initialize Context using selected API
+	m_OpenCLSubmission = useOpenCL;
+	switch (m_API->GetType()) {
+		case API::Type::Host:
+			m_AMFMemoryType = amf::AMF_MEMORY_HOST;
+			m_OpenCLSubmission = false;
+			break;
+		case API::Type::OpenGL:
+			m_AMFMemoryType = amf::AMF_MEMORY_OPENGL;
+			res = m_AMFContext->InitOpenGL(m_API->GetContextFromInstance(m_APIDevice), 0, 0);
+			break;
+		case API::Type::Direct3D9:
+			m_AMFMemoryType = amf::AMF_MEMORY_DX9;
+			res = m_AMFContext->InitDX9(m_API->GetContextFromInstance(m_APIDevice));
+			break;
+		case API::Type::Direct3D11:
+			m_AMFMemoryType = amf::AMF_MEMORY_DX11;
+			res = m_AMFContext->InitDX11(m_API->GetContextFromInstance(m_APIDevice));
+			break;
+	}
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Initializing %s API with Adapter '%s' failed, error %ls (code %d).",
+			m_UniqueId,
+			m_API->GetName().c_str(), m_APIAdapter.Name.c_str(),
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+
+	// Initialize OpenCL (if possible)
+	res = m_AMFContext->InitOpenCL();
+	if (res != AMF_OK) {
+		m_OpenCLSubmission = false;
+		m_OpenCLConversion = false;
+
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Initialising OpenCL failed, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		AMF_LOG_WARNING("%s", errMsg.data());
+	} else {
+		m_OpenCLConversion = true;
+
+		if (m_OpenCLSubmission) {
+			res = m_AMFContext->GetCompute(amf::AMF_MEMORY_OPENCL, &m_AMFCompute);
+			if (res != AMF_OK) {
+				m_OpenCLSubmission = false;
+
+				QUICK_FORMAT_MESSAGE(errMsg,
+					"<Id: %lld> Retrieving Compute object failed, error %ls (code %d)",
+					m_UniqueId,
+					m_AMF->GetTrace()->GetResultText(res), res);
+				AMF_LOG_WARNING("%s", errMsg.data());
+			}
+		}
+	}
+
+	// Create Converter
+	res = m_AMFFactory->CreateComponent(m_AMFContext, AMFVideoConverter, &m_AMFConverter);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Creating frame converter component failed, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	res = m_AMFConverter->SetProperty(AMF_VIDEO_CONVERTER_MEMORY_TYPE, m_AMFMemoryType);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to set converter memory type, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	res = m_AMFConverter->SetProperty(AMF_VIDEO_CONVERTER_OUTPUT_FORMAT, amf::AMF_SURFACE_NV12);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to set converter output format, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	res = m_AMFConverter->SetProperty(AMF_VIDEO_CONVERTER_COLOR_PROFILE, Utility::ColorSpaceToAMFConverter(m_ColorSpace));
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to set convertor color profile, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	for (const wchar_t* par : fullColorParams) {
+		res = m_AMFConverter->SetProperty(par, m_FullColorRange);
+		if (res == AMF_OK)
+			break;
+	}
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to set converter color range, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	res = m_AMFConverter->Init(amf::AMF_SURFACE_NV12, 0, 0);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to intialize converter, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res),
+			res);
+		throw std::exception(errMsg.data());
+	}
+
+	// Create Encoder
+	res = m_AMFFactory->CreateComponent(m_AMFContext, Utility::CodecToAMF(codec), &m_AMFEncoder);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Unable to create %s encoder, error %ls (code %d)",
+			m_UniqueId,
+			Utility::CodecToString(codec),
+			m_AMF->GetTrace()->GetResultText(res),
+			res);
+		throw std::exception(errMsg.data());
+	}
+
+	// Full Range Color Stuff
+	for (const wchar_t* par : fullColorParams) {
+		res = m_AMFEncoder->SetProperty(par, m_FullColorRange);
+		if (res == AMF_OK)
+			break;
+	}
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Failed to set encoder color range, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+
+	// Show complete initialization in log.
+	QUICK_FORMAT_MESSAGE(notice,
+		"<Id: %lld> Initialized.",
+		m_UniqueId);
+	AMF_LOG_DEBUG("%s", notice.data());
+}
+
+Plugin::AMD::Encoder::~Encoder() {
+	// Destroy AMF Encoder
+	if (m_AMFEncoder) {
+		m_AMFEncoder->Terminate();
+		m_AMFEncoder = nullptr;
+	}
+
+	// Destroy AMF Converter
+	if (m_AMFConverter) {
+		m_AMFConverter->Terminate();
+		m_AMFConverter = nullptr;
+	}
+
+	// Destroy AMF Context
+	if (m_AMFContext) {
+		m_AMFContext->Terminate();
+		m_AMFContext = nullptr;
+	}
+
+	// Destroy API
+	if (m_API) {
+		m_API->DestroyInstance(m_APIDevice);
+		m_APIDevice = nullptr;
+		m_API = nullptr;
+	}
+
+	m_AMF = nullptr;
+
+	// Show complete initialization in log.
+	QUICK_FORMAT_MESSAGE(notice,
+		"<Id: %lld> Finalized.",
+		m_UniqueId);
+	AMF_LOG_DEBUG("%s", notice.data());
+}
+
+void Plugin::AMD::Encoder::UpdateFrameRateValues() {
+	// 1			Second
+	// 1000			Millisecond
+	// 1000000		Microsecond
+	// 10000000		amf_pts
+	// 1000000000	Nanosecond
+	m_FrameRateTimeStep = 1.0 / ((double_t)m_FrameRate.first / (double_t)m_FrameRate.second);
+	m_FrameRateTimeStepAMF = (uint64_t)round(m_FrameRateTimeStep * AMF_SECOND);
+}
+
+void Plugin::AMD::Encoder::Start() {
+	AMF_RESULT res = m_AMFEncoder->Init(Utility::ColorFormatToAMF(m_ColorFormat), m_Resolution.first, m_Resolution.second);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Failed to initialize encoder, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+	m_Started = true;
+}
+
+void Plugin::AMD::Encoder::Restart() {
+	AMF_RESULT res = m_AMFEncoder->ReInit(m_Resolution.first, m_Resolution.second);
+	if (res != AMF_OK) {
+		QUICK_FORMAT_MESSAGE(errMsg,
+			"<Id: %lld> Could not re-initialize encoder, error %ls (code %d)",
+			m_UniqueId,
+			m_AMF->GetTrace()->GetResultText(res), res);
+		throw std::exception(errMsg.data());
+	}
+}
+
+void Plugin::AMD::Encoder::Stop() {
+	if (!m_Started)
+		throw std::logic_error("Can't stop an encoder that isn't running!");
+
+	m_AMFConverter->Drain();
+	m_AMFConverter->Flush();
+	m_AMFEncoder->Drain();
+	m_AMFEncoder->Flush();
+
+	m_Started = false;
+}
+
+bool Plugin::AMD::Encoder::Encode(struct encoder_frame* frame, struct encoder_packet* packet, bool* received_packet) {
+	if (!m_Started)
+		return false;
+
+	AMF_RESULT res;
+	amf::AMFSurfacePtr pSurface = nullptr;
+	amf::AMFDataPtr pData = nullptr;
+
+	// Allocate Surface
+	{
+		res = m_AMFContext->AllocSurface(m_AMFMemoryType, m_AMFSurfaceFormat,
+			m_Resolution.first, m_Resolution.second, &pSurface);
+		if (res != AMF_OK) {
+			QUICK_FORMAT_MESSAGE(errMsg,
+				"<Id: %lld> Unable to allocate Surface, error %ls (code %d)",
+				m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+			AMF_LOG_WARNING("%s", errMsg.data());
+			return false;
+		}
+	}
+
+	// Copy Information
+	{
+		amf::AMFComputeSyncPointPtr pSyncPoint;
+		if (m_OpenCLSubmission) {
+			m_AMFCompute->PutSyncPoint(&pSyncPoint);
+			res = pSurface->Convert(amf::AMF_MEMORY_OPENCL);
+			if (res != AMF_OK) {
+				QUICK_FORMAT_MESSAGE(errMsg,
+					"<Id: %lld> Conversion of Surface to OpenCL failed, error %ls (code %d)",
+					m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+				AMF_LOG_WARNING("%s", errMsg.data());
+				return false;
+			}
+		}
+
+		size_t planeCount = pSurface->GetPlanesCount();
+		for (uint8_t i = 0; i < planeCount; i++) {
+			amf::AMFPlanePtr plane = pSurface->GetPlaneAt(i);
+			int32_t width = plane->GetWidth();
+			int32_t height = plane->GetHeight();
+			int32_t hpitch = plane->GetHPitch();
+
+			if (m_OpenCLSubmission) {
+				static const amf_size l_origin[] = { 0, 0, 0 };
+				const amf_size l_size[] = { (amf_size)width, (amf_size)height, 1 };
+				res = m_AMFCompute->CopyPlaneFromHost(frame->data[i], l_origin, l_size, frame->linesize[i], pSurface->GetPlaneAt(i), false);
+				if (res != AMF_OK) {
+					QUICK_FORMAT_MESSAGE(errMsg,
+						"<Id: %lld> Unable to copy plane %d with OpenCL, error %ls (code %d)",
+						m_UniqueId, i, m_AMF->GetTrace()->GetResultText(res), res);
+					AMF_LOG_WARNING("%s", errMsg.data());
+					return false;
+				}
+			} else {
+				void* plane_nat = plane->GetNative();
+				#pragma loop(hint_parallel(32))
+				for (int32_t py = 0; py < height; py++) {
+					int32_t plane_off = py * hpitch;
+					int32_t frame_off = py * frame->linesize[i];
+					std::memcpy(
+						static_cast<void*>(static_cast<uint8_t*>(plane_nat) + plane_off),
+						static_cast<void*>(frame->data[i] + frame_off), frame->linesize[i]);
+				}
+			}
+		}
+
+		if (m_OpenCLSubmission) {
+			res = m_AMFCompute->FinishQueue();
+			if (res != AMF_OK) {
+				QUICK_FORMAT_MESSAGE(errMsg,
+					"<Id: %lld> Failed to finish OpenCL queue, error %ls (code %d)",
+					m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+				AMF_LOG_WARNING("%s", errMsg.data());
+				return false;
+			}
+			pSyncPoint->Wait();
+			res = pSurface->Convert(m_AMFMemoryType);
+			if (res != AMF_OK) {
+				QUICK_FORMAT_MESSAGE(errMsg,
+					"<Id: %lld> Conversion of Surface from OpenCL failed, error %ls (code %d)",
+					m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+				AMF_LOG_WARNING("%s", errMsg.data());
+				return false;
+			}
+		}
+	}
+
+	// Color Conversion
+	if (m_OpenCLConversion) {
+		pSurface->Convert(amf::AMF_MEMORY_OPENCL);
+		m_AMFConverter->SubmitInput(pSurface);
+		m_AMFConverter->QueryOutput(&pData);
+		pSurface->Convert(m_AMFMemoryType);
+	}
+
+	// Submit Frame
+	{
+		if (pData != nullptr)
+			pSurface = (amf::AMFSurfacePtr)pData;
+		/// Presentation Timestamp
+		pSurface->SetPts(frame->pts * m_FrameRateTimeStepAMF);
+		/// Decode Timestamp
+		pSurface->SetProperty(AMF_PRESENT_TIMESTAMP, frame->pts - 2); // -2 Offset for B-Picture Support
+		/// Duration
+		pSurface->SetDuration((uint64_t)round(frame->pts * m_FrameRateTimeStep * AMF_SECOND));
+		/// Performance Monitoring: Submission Timestamp
+		auto clk = std::chrono::high_resolution_clock::now();
+		pSurface->SetProperty(AMF_SUBMIT_TIMESTAMP, std::chrono::nanoseconds(clk.time_since_epoch()).count());
+		/// Submit
+		res = m_AMFEncoder->SubmitInput(pSurface);
+		switch (res) {
+			default:
+				{
+					QUICK_FORMAT_MESSAGE(errMsg,
+						"<Id: %lld> Submitting Surface failed, error %ls (code %d)",
+						m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+					AMF_LOG_ERROR("%s", errMsg.data());
+				}
+				return false;
+			case AMF_INPUT_FULL:
+				{
+					QUICK_FORMAT_MESSAGE(errMsg,
+						"<Id: %lld> Submitting Surface failed , error %ls (code %d)",
+						m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+					AMF_LOG_ERROR("%s", errMsg.data());
+				}
+				break;
+			case AMF_EOF:
+				break; // Swallow
+		}
+	}
+
+	// Retrieve Frame
+	{
+		res = m_AMFEncoder->QueryOutput(&pData);
+		switch (res) {
+			default:
+				{
+					QUICK_FORMAT_MESSAGE(errMsg,
+						"<Id: %lld> Retrieving Packet failed, error %ls (code %d)",
+						m_UniqueId, m_AMF->GetTrace()->GetResultText(res), res);
+					AMF_LOG_ERROR("%s", errMsg.data());
+				}
+				return false;
+			case AMF_OK:
+				{
+					amf::AMFBufferPtr pBuffer = amf::AMFBufferPtr(pData);
+
+					packet->type = OBS_ENCODER_VIDEO;
+					packet->size = pBuffer->GetSize();
+					if (m_PacketDataBuffer.size() < packet->size) {
+						size_t newBufferSize = (size_t)exp2(ceil(log2(packet->size)));
+						//AMF_LOG_DEBUG("Packet Buffer was resized to %d byte from %d byte.", newBufferSize, m_PacketDataBuffer.size());
+						m_PacketDataBuffer.resize(newBufferSize);
+					}
+					packet->data = m_PacketDataBuffer.data();
+					std::memcpy(packet->data, pBuffer->GetNative(), packet->size); // ToDo: Can we make this threaded?
+					packet->dts = pData->GetPts() / m_FrameRateTimeStepAMF;
+					pData->GetProperty(AMF_PRESENT_TIMESTAMP, &packet->pts); // Not technically needed, but VLC is stupid. :(
+
+					{
+						uint64_t pktType;
+						if (m_Codec != Codec::HEVC) {
+							pData->GetProperty(AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE, &pktType);
+							switch ((AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE_ENUM)pktType) {
+								case AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE_IDR:
+								case AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE_I:
+									packet->keyframe = true;
+									packet->priority = 3;
+									break;
+								case AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE_P:
+									packet->priority = 2;
+									break;
+								case AMF_VIDEO_ENCODER_OUTPUT_DATA_TYPE_B:
+									packet->priority = 0;
+									break;
+							}
+						} else {
+							pData->GetProperty(AMF_VIDEO_ENCODER_HEVC_OUTPUT_DATA_TYPE, &pktType);
+							switch ((AMF_VIDEO_ENCODER_HEVC_OUTPUT_DATA_TYPE_ENUM)pktType) {
+								case AMF_VIDEO_ENCODER_HEVC_OUTPUT_DATA_TYPE_I:
+									packet->keyframe = true;
+									packet->priority = 3;
+									break;
+								case AMF_VIDEO_ENCODER_HEVC_OUTPUT_DATA_TYPE_P:
+									packet->priority = 1;
+									break;
+							}
+						}
+					}
+
+					*received_packet = true;
+				}
+				break;
+			case AMF_REPEAT: // These two just mean that we need to submit more and aren't actually errors.
+			case AMF_NEED_MORE_INPUT:
+			case AMF_EOF: // Encoder is done encoding.
+				break; // Swallow
+		}
+	}
+
+	return true;
+}
+
