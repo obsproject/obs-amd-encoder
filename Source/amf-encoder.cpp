361c7f54b9d35ac9de4eb59a7b23c132cf51da84
Michael Fabian Dirks <info@xaymar.com>
2017-02-07 07:28:12 +0100

    project: Update LICENSE text for 2017

--- Source/amf-encoder.cpp
+++ Source/amf-encoder.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2016 Michael Fabian Dirks
+Copyright (c) 2016-2017
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
