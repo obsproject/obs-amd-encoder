5be6602288ac56318113aabe2bd6a23363d6e52e
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2018-09-04 03:54:10 +0200

    ci: CMake Packages and renamed packages to obs-amd-encoder

--- ci/appveyor-package.bat
+++ ci/appveyor-package.bat
@@ -1,7 +1,6 @@
-cd /D build\distrib
 ECHO -- Building 7z Archive --
-7z a -t7z -mx=9 -m0=lzma2 -aoa -mfb=64 -md=32m -ms=on "../%PACKAGE_NAME%.7z" * > nul
+cmake --build build/64 --target PACKAGE_7Z --config RelWithDebInfo
 ECHO -- Building Zip Archive --
-7z a -tzip -mx=9 "../%PACKAGE_NAME%.zip" * > nul
+cmake --build build/64 --target PACKAGE_ZIP --config RelWithDebInfo
 ECHO -- Building Installer --
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp "..\64\ci\installer.iss" > nul
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp ".\build\64\ci\installer.iss" > nul
